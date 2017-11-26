0d79fe5375ed2733ccaed91740d69a8a048fab8f
Administrator <Administrator@XGATE-PC>
2017-11-27 01:27:01 +0900

    초기 커밋.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace auto_trade
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var json = new WebClient().DownloadString("https://bittrex.com/api/v1.1/public/getmarkets");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var json = new WebClient().DownloadString("https://bittrex.com/api/v1.1/public/getmarkets");
+
+            using (var context = new tradeEntities())
+            {
+                var ex = context.exchanges;
+                foreach (var item in ex)
+                {
+                    listTest.Items.Add(item.name);
+                }
+            }
+        }
+    }
+}
---
+++ Model1.edmx.sql
@@ -0,0 +1,149 @@
+
+-- --------------------------------------------------
+-- Entity Designer DDL Script for SQL Server 2005, 2008, 2012 and Azure
+-- --------------------------------------------------
+-- Date Created: 11/27/2017 00:38:42
+-- Generated from EDMX file: C:\dev\source\auto_trade\Model1.edmx
+-- --------------------------------------------------
+
+SET QUOTED_IDENTIFIER OFF;
+GO
+USE [trade];
+GO
+IF SCHEMA_ID(N'dbo') IS NULL EXECUTE(N'CREATE SCHEMA [dbo]');
+GO
+
+-- --------------------------------------------------
+-- Dropping existing FOREIGN KEY constraints
+-- --------------------------------------------------
+
+
+-- --------------------------------------------------
+-- Dropping existing tables
+-- --------------------------------------------------
+
+IF OBJECT_ID(N'[dbo].[exchanges]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[exchanges];
+GO
+IF OBJECT_ID(N'[dbo].[markets]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[markets];
+GO
+IF OBJECT_ID(N'[dbo].[my_orders]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[my_orders];
+GO
+IF OBJECT_ID(N'[dbo].[orders]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[orders];
+GO
+IF OBJECT_ID(N'[dbo].[price_hist]', 'U') IS NOT NULL
+    DROP TABLE [dbo].[price_hist];
+GO
+
+-- --------------------------------------------------
+-- Creating all tables
+-- --------------------------------------------------
+
+-- Creating table 'exchanges'
+CREATE TABLE [dbo].[exchanges] (
+    [id] int IDENTITY(1,1) NOT NULL,
+    [name] nvarchar(100)  NULL,
+    [key] nvarchar(500)  NULL
+);
+GO
+
+-- Creating table 'markets'
+CREATE TABLE [dbo].[markets] (
+    [id] int IDENTITY(1,1) NOT NULL,
+    [exchange_id] int  NULL,
+    [currency] nvarchar(20)  NULL,
+    [currency_long] nvarchar(50)  NULL,
+    [base_currency] nvarchar(20)  NULL,
+    [min_trade_size] decimal(11,8)  NULL,
+    [market_name] nvarchar(20)  NULL,
+    [is_active] bit  NULL,
+    [created_at] datetime  NULL
+);
+GO
+
+-- Creating table 'my_orders'
+CREATE TABLE [dbo].[my_orders] (
+    [id] int IDENTITY(1,1) NOT NULL,
+    [market_id] int  NULL,
+    [order_uuid] nvarchar(50)  NULL,
+    [order_type] nvarchar(20)  NULL,
+    [quantity] decimal(11,8)  NULL,
+    [quantity_remain] decimal(11,8)  NULL,
+    [reserved] decimal(11,8)  NULL,
+    [reserved_remaining] decimal(11,8)  NULL,
+    [commission_reserved] decimal(11,8)  NULL,
+    [commission_reserved_remaining] decimal(11,8)  NULL,
+    [commission_paid] decimal(11,8)  NULL,
+    [price] decimal(11,8)  NULL,
+    [opened_at] datetime  NULL,
+    [closed_at] datetime  NULL
+);
+GO
+
+-- Creating table 'orders'
+CREATE TABLE [dbo].[orders] (
+    [id] int IDENTITY(1,1) NOT NULL,
+    [market_id] int  NULL,
+    [type] bit  NULL,
+    [quantity] decimal(11,8)  NULL,
+    [rate] decimal(11,8)  NULL
+);
+GO
+
+-- Creating table 'price_hist'
+CREATE TABLE [dbo].[price_hist] (
+    [id] int IDENTITY(1,1) NOT NULL,
+    [market_id] int  NULL,
+    [bid] decimal(11,8)  NULL,
+    [ask] decimal(11,8)  NULL,
+    [last] decimal(11,8)  NULL,
+    [avg] decimal(11,8)  NULL,
+    [acc_rate] int  NULL,
+    [created_at] datetime  NULL
+);
+GO
+
+-- --------------------------------------------------
+-- Creating all PRIMARY KEY constraints
+-- --------------------------------------------------
+
+-- Creating primary key on [id] in table 'exchanges'
+ALTER TABLE [dbo].[exchanges]
+ADD CONSTRAINT [PK_exchanges]
+    PRIMARY KEY CLUSTERED ([id] ASC);
+GO
+
+-- Creating primary key on [id] in table 'markets'
+ALTER TABLE [dbo].[markets]
+ADD CONSTRAINT [PK_markets]
+    PRIMARY KEY CLUSTERED ([id] ASC);
+GO
+
+-- Creating primary key on [id] in table 'my_orders'
+ALTER TABLE [dbo].[my_orders]
+ADD CONSTRAINT [PK_my_orders]
+    PRIMARY KEY CLUSTERED ([id] ASC);
+GO
+
+-- Creating primary key on [id] in table 'orders'
+ALTER TABLE [dbo].[orders]
+ADD CONSTRAINT [PK_orders]
+    PRIMARY KEY CLUSTERED ([id] ASC);
+GO
+
+-- Creating primary key on [id] in table 'price_hist'
+ALTER TABLE [dbo].[price_hist]
+ADD CONSTRAINT [PK_price_hist]
+    PRIMARY KEY CLUSTERED ([id] ASC);
+GO
+
+-- --------------------------------------------------
+-- Creating all FOREIGN KEY constraints
+-- --------------------------------------------------
+
+-- --------------------------------------------------
+-- Script has ended
+-- --------------------------------------------------
